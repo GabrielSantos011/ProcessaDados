6df0a3570eb5becb2ceb0455fce9c0b94f56628c
Gabriel <gabrielrafael.grs@gmail.com>
2026-08-24 00:49:41 -0300

    report

---
+++ src/main/java/br/com/gabriel/processaDados/ReportMain.java
@@ -0,0 +1,11 @@
+import br.com.gabriel.processaDados.util.TransactionReport;
+
+void main() {
+    var transactionReport = new TransactionReport();
+    TransactionReport.Statistics statistics = transactionReport.generateReport("data/PS_20174392719_1491204439457_log.csv");
+    IO.println("""
+        Total de linhas: %d
+        Total de fraudes: %d
+        Valor total transacionado: %.2f
+        """.formatted(statistics.totalTransactions(), statistics.totalFrauds(), statistics.totalAmount()));
+}
