2069674e7c847b4cad3413d1aaa35f413b3ebf30
Gabriel <gabrielrafael.grs@gmail.com>
2026-08-25 00:03:13 -0300

    i18n - internacionalização

---
+++ src/main/java/br/com/gabriel/processaDados/I18NMain.java
@@ -0,0 +1,33 @@
+import br.com.gabriel.processaDados.util.TransactionReport;
+
+void main(String[] args) {
+    String language = (args.length > 0 ? args[0]: "pt");
+    var locale = Locale.of(language);
+
+    var integerFormatter = NumberFormat.getIntegerInstance(locale);
+    var currencyFormatter = DecimalFormat.getCurrencyInstance(locale);
+    currencyFormatter.setCurrency(Currency.getInstance("USD"));
+
+    var resourceBundle = ResourceBundle.getBundle("report", locale);
+
+    var transactionReport = new TransactionReport();
+    TransactionReport.Statistics statistics = transactionReport.generateReport("data/PS_20174392719_1491204439457_log.csv");
+
+    String fmtTotalTransactions = integerFormatter.format(statistics.totalTransactions());
+    String fmtTotalFrauds = integerFormatter.format(statistics.totalFrauds());
+    String fmtTotalAmount = currencyFormatter.format(statistics.totalAmount());
+
+    String msgTotalTransactions = resourceBundle.getString("label.total.transactions");
+    String msgTotalFrauds = resourceBundle.getString("label.total.frauds");
+    String msgTotalAmount = resourceBundle.getString("label.total.amount");
+
+    IO.println("""
+        %s: %s
+        %s: %s
+        %s: %s
+        """.formatted(
+            msgTotalTransactions, fmtTotalTransactions,
+            msgTotalFrauds, fmtTotalFrauds,
+            msgTotalAmount, fmtTotalAmount
+    ));
+}
